e4e10352d2fa320f5bf75c8b9ba9f1fb3e1a3b9c
Ренат <renat1703@gmail.com>
2023-03-12 19:17:05 +0300

    Add method enterprisePageCheckTest

---
+++ src/test/java/GitHubTest.java
@@ -0,0 +1,25 @@
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selenide.*;
+
+public class GitHubTest {
+    @BeforeAll
+    static void beforeAll() {
+        Configuration.browserSize = "1920x1080";
+    }
+
+    @Test
+    void enterprisePageCheckTest() {
+        open("https://github.com/");
+
+        $$(".HeaderMenu-link").findBy(text("Solutions")).hover();
+        $$(".HeaderMenu-dropdown-link").findBy(text("Enterprise")).click();
+        $(".enterprise-hero ").shouldHave(text("Build like the best"));
+        sleep(5000);
+
+
+    }
+}
